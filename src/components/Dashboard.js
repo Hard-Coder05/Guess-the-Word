9d2051bef7efc6e17e1c63e8fed27f0c4b3ec137
Siddharth Mishra <smishra1605@gmail.com>
2022-03-19 16:17:29 +0530

    completed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,15 +4,19 @@ import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
 import Header from "./UI/Header"
 import styles from './Dashboard.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Dashboard () {
-  const [score, setScore] = useState(400);
+  const [score, setScore] = useState(600);
   const [level, setLevel] = useState("");
   const [win, setWin] = useState(0);
+  const [computerPoints, setComputerPoints] = useState(600);
   const [currentWord, setCurrentWord] = useState("");
   const [currentWordJumbled, setCurrentWordJumbled] = useState([]);
   const [jumbledLetterComponentsList, setjumbledLetterComponentsList] = useState([]);
   const [currentUserAnswer, setCurrentUserAnswer] = useState([]);
   const [currentUserAnswerComponentList, setCurrentUserAnswerComponentList] = useState([]);
+  const [isUserAnswerValid, setUserAnswerValid] = useState(false);
 
   // styling for letters
   var letterStyle = {
@@ -68,7 +72,7 @@ export default function Dashboard () {
         setCurrentUserAnswer([]);
       });
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
-  }, [win]);
+  }, [score, win]);
 
   // jumble the word received
   useEffect(() => {
@@ -138,25 +142,51 @@ export default function Dashboard () {
 
   const { currentUser } = useAuth();
 
-  const buttonClickHandler = () => {
+  async function buttonClickHandler () {
     var userFinalAnswer = currentUserAnswer.join('');
-    if (userFinalAnswer === currentWord.toString()) {
-      setScore(score + 50);
+    var url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+    url = url.concat(userFinalAnswer.toString());
+    const response = await fetch(url);
+    const data = await response.json();
+    if (userFinalAnswer.toString() === currentWord.toString()) {
+      setScore(score + 20 * (userFinalAnswer.length));
+      setComputerPoints(computerPoints - 20 * (userFinalAnswer.length));
       setWin(win + 1);
+      toast("Congratulations!! You entered a new round");
+    }
+    else if (Array.isArray(data) && data[0].word === userFinalAnswer.toString()) {
+      if (userFinalAnswer.length > currentWord.toString().length) {
+        setScore(score + 20 * (userFinalAnswer.length));
+        setComputerPoints(computerPoints - 20 * (userFinalAnswer.length));
+        setWin(win + 1);
+        toast("Congratulations!! You entered a new round");
+      }
+      else {
+        toast("Sorry!! Computer has a longer word in head");
+      }
     }
     else {
-      setWin(win - 1);
-      setScore(score - 30);
+      toast("Heyy! Type in a meaningfull word");
     }
   }
   return (
     <>
       <Header />
       <hr className={styles.hrStyle} />
+      <Row className={styles.gameStats}>
+        <Col >
+          Hi {currentUser.email} !!!
+        </Col>
+        <Col >
+          Computer Health: {computerPoints}
+        </Col>
+      </Row>
+      <hr className={styles.hrStyle} />
       <div className={styles.game}>
+        <ToastContainer />
         <Row className={styles.gameStats}>
           <Col >
-            User Score: {score}
+            User Health: {score}
           </Col>
           <Col >
             Level: {level}
@@ -173,9 +203,21 @@ export default function Dashboard () {
           <div className={styles.userAnswer}>
             {currentUserAnswerComponentList}
           </div>
-          <Button onClick={buttonClickHandler}>
-            Check if Correct
-          </Button>
+          <Row>
+            <Col>
+              <Button onClick={buttonClickHandler}>
+                Attack Computer
+              </Button>
+            </Col>
+            <Col>
+              <Button onClick={() => {
+                setScore(score - 20 * (currentWord.toString().length));
+              }}>
+                Skip
+              </Button>
+            </Col>
+          </Row>
+
         </>}
         {currentWordJumbled.length > 0 && <>
           <hr className={styles.hrStyle} />
